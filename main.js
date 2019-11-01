fbe44923c6fdd2dfafc474a482aaef9fa2660d4f
jluizrope <47804079+jluizrope@users.noreply.github.com>
2019-11-01 12:19:03 -0300

    Fiz o terceiro desafio, uma lista de alunos, hoje

---
+++ main.js
@@ -0,0 +1,111 @@
+var array = [
+{
+	nome: 'Cícero',
+	idade: 24,
+	Polo: 'Morro dos Prazeres',	
+},
+{
+	nome: 'Cássio',
+	idade: 21,
+	Polo: 'Morro dos Prazeres'
+},
+
+{
+	nome: 'Douglas',
+	idade: 19,
+	Polo: 'Complexo do Alemão',
+},
+{
+	nome: 'Priscila',
+	idade: 32,
+	Polo: 'Morro dos Prazeres',
+},
+{
+	nome: 'Arthur',
+	idade: 24,
+	Polo: 'Complexo do Alemão',
+},
+{
+	nome: 'Caroline',
+	idade: 19,
+	Polo: 'Morro dos Prazeres',
+},
+{
+	nome: 'Cleiton',
+	idade: 19,
+	Polo: 'Complexo do Alemão',
+},
+{
+	nome: 'Dandara'
+	idade: 21,
+	Polo: 'Complexo do Alemão',
+},
+{
+	nome: 'Erlane',
+	idade: 24,
+	Polo: 'Morro dos Prazeres',
+},
+{	nome: 'Estefania',
+	idade: 22,
+	Polo: 'Morro dos Prazeres',	 
+},
+{
+	nome: 'Geysa',
+	idade: 20,
+	Polo: 'Morro dos Prazeres',
+},
+{
+	nome: 'João',
+	idade: 19,
+	Polo: 'Complexo do Alemão',
+},
+{
+	nome: 'Jorge Luiz',
+	idade: 58,
+	Polo: 'Complexo dos Prazeres', 
+},
+{
+	nome: 'Luan',
+	idade: 25,
+	Polo: 'Complexo do Alemão',
+},
+{
+	nome: 'Lucas',
+	idade: 24,
+	Polo: 'Morro do Alemão',
+},
+{
+	nome: 'Maicon',
+	idade: 23,
+	Polo: 'Complexo do Alemão',
+},
+{
+	nome: 'Patrícia',
+	idade: 32,
+	Polo: 'Complexo do Alemão',
+},
+{
+	nome: 'Renan',
+	idade: 15,
+	Polo: 'Morro dos Prazeres',
+},
+{
+	nome: 'Rhuan',
+	idade: 20,
+	Polo: 'Morro dos Prazeres',
+},
+{
+	nome: 'Victor Hugo',
+	idade: 24,
+	Polo: 'Complexo do Alemão',
+},
+{
+	nome: 'Zaíra',
+	idade: 19,
+	Polo: 'Morro dos Prazeres',
+},
+]
+
+
+
+
